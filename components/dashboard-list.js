8a45feb1b8dc11aecc25e33c9bd8c4bb4729b82f
Ryan Kent <imsolost@gmail.com>
2023-08-10 11:50:06 -0700

    added things to dashboard

--- components/dashboard-list.js
+++ components/dashboard-list.js
@@ -6,11 +6,36 @@ const StyledDashboard = styled.div`
   background-color: #f5f5f5;
   width: 200px;
   height: 100vh;
-`;
+`
+
+const GreyShape = styled.div`
+  position: relative;
+  border-radius: 22px;
+  background-color: #d6d6d6;
+  width: 115px;
+  height: 20px;
+`
+
+const ListItem = styled.div`
+  display: flex;
+  margin-top: 10px;
+`
+
+const listImages = ['layout', 'inbox', 'mail', 'star', 'filetext', 'grid', 'settings', 'user']
+
 const DashboardList = () => {
+  const listItems = listImages.map((image, id) => 
+      <ListItem>
+        <img alt={image} src={`/${image}.svg`}/>
+        <GreyShape />
+      </ListItem>
+    )
+
   return (
     <StyledDashboard>
+      <img alt="" src="/home.svg"/>
       Dashboard
+      {listItems}      
     </StyledDashboard>
   );
 };
